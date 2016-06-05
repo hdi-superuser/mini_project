b5c617ceefff6f1022b463fe1b6fea00f4777df9
Duy Huynh <huynhduy.idol@gmail.com>
2016-06-06 05:39:11 +0700

    update main.cpp

--- subset/main.cpp
+++ subset/main.cpp
@@ -82,38 +82,10 @@ void process() {
 	}
 }
 
-void check() {
-    ifstream f_check("out.txt");
-    ofstream f_log("log.txt");
-    string str;
-    str = "";
-    ll temp = 0;
-    int cnt = 0, ok = 0;
-    while (str != "eof.") {
-        f_check >> str;
-        if (str[0] == 'T') break;
-        int n = 0;
-        int p = str.find('-');
-        for (int i = p+1; i < str.size()-1; i++) n = n*10 + str[i] - 48;
-        ll x;
-        temp = 0;
-        for (int i = 1; i <= n; i++) {
-            f_check >> x;
-            temp += x;
-        }
-        f_log << "#" << ++cnt << ": ";
-        if (temp == S) f_log << "Ok" << endl, ok++;
-        else f_log << "Wrong" << endl;
-    }
-    f_log << ok << " ok, " << cnt - ok << " wrong.";
-    f_log << endl << "eof.";
-}
-
 int main() {
 	enter_data();
 	process();
 	f_out << "There are " << Count << " subset whose sum is equal to " << S << ".";
 	f_out << endl << "eof.";
-	check();
 	return 0;
 }
